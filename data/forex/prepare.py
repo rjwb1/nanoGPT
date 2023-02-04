9fe97ce4e7c4a32067ecaaaefbf8df726a377dd6
rjwb1 <rbelshaw@sagarobotics.com>
2023-02-04 16:14:33 +0000

    data prepare

--- data/forex/prepare.py
+++ data/forex/prepare.py
@@ -1,9 +1,11 @@
 import os, sys
 import numpy as np
 import csv 
+import pickle
 
 data_file = "EURUSD_H1.csv"
-data_index = 4
+data_index = 4 
+vocab_size = 50257
 data = []
 # Import Forex Data from CSV
 input_file_path = os.path.join(os.path.dirname(__file__), data_file)
@@ -11,22 +13,35 @@ if not os.path.exists(input_file_path):
     print("forex csv not found: %s" % data_file)
     sys.exit()
 
+previous_data_point = 0
+
 with open(input_file_path, 'r') as f:
     csv_file = csv.reader(f)
     for i, row in enumerate(csv_file):
-        data.append(row[4]) 
+        if i == 0:
+            previous_price = float(row[4])
+        else:
+            data.append(float(row[4]) - float(previous_price)) 
+            previous_price = float(row[4])
+
 n = len(data)
 
 data_min = min(data)
 data_max = max(data)
 
+print(f"data has minimum change of = {data_min:,}")
+print(f"data has max change of {data_max:,}")
+
+steps = (data_max - data_min) / vocab_size
+bins = np.arange(data_min, data_max, steps) 
+data = np.digitize(data, bins)
+
 train_data = data[:int(n*0.9)]
 val_data = data[int(n*0.9):]
 
-# encode with tiktoken gpt2 bpe
-# enc = tiktoken.get_encoding("gpt2")
-# train_ids = enc.encode_ordinary(train_data)
-# val_ids = enc.encode_ordinary(val_data)
+train_ids = train_data
+val_ids = val_data
+
 print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
 
@@ -36,5 +51,11 @@ val_ids = np.array(val_ids, dtype=np.uint16)
 train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
 val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
 
-# train.bin has 301,966 tokens
-# val.bin has 36,059 tokens
+# save the meta information as well, to help us encode/decode later
+meta = {
+    'vocab_size': vocab_size,
+    'itos': None,
+    'stoi': None,
+}
+with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
+    pickle.dump(meta, f)
--- train.py
+++ train.py
@@ -29,6 +29,9 @@ from torch.distributed import init_process_group, destroy_process_group
 
 from model import GPTConfig, GPT
 
+from pathlib import Path
+
+os.chdir(Path(__file__).parent)
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
@@ -40,11 +43,11 @@ eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
-wandb_log = False # disabled by default
+wandb_log = True # disabled by default
 wandb_project = 'owt'
 wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
-dataset = 'openwebtext'
+dataset = 'forex'
 gradient_accumulation_steps = 1 # used to simulate larger batch sizes
 batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 1024
@@ -71,7 +74,7 @@ backend = 'nccl' # 'nccl', 'gloo', etc.
 # system
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
 dtype = 'bfloat16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
-compile = True # use PyTorch 2.0 to compile the model to be faster
+compile = False# use PyTorch 2.0 to compile the model to be faster
 # -----------------------------------------------------------------------------
 config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
 exec(open('configurator.py').read()) # overrides from command line or config file
